222d7b4c1e81d4bc9ea8f812645706bfd3ec7497
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-27 00:35:54 -0300

    perf: insert method class list contains and condition

--- fac/script.js
+++ fac/script.js
@@ -2,8 +2,13 @@ const lista = document.querySelectorAll('div.question')
 
 for (let i = 0; i < lista.length; i++) {
   lista[i].addEventListener('click', function (event) {
-    removeClass()
-    addClass(event)
+
+    if (event.target.classList.contains('active')) {
+      removeClass()
+    } else {
+      removeClass()
+      addClass(event)
+    }
   })
 }
 
