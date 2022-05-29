3e2b9519477fa9850a3c0ff692367f5ffe8e8c94
Júlia Ohara <juliaohara.br@gmail.com>
2022-05-28 23:54:20 -0300

    feat: insert buttons to remove items from list

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,18 +1,59 @@
-const form = document.querySelector('.register');
-const addItem = document.querySelector('.addItem');
-const taskInput = document.querySelector('.taskInput');
-const list = document.querySelector('.list');
+const form = document.querySelector(".register");
+const taskInput = document.querySelector(".taskInput");
+const list = document.querySelector(".list");
 
-let task = ['Estudar JavaScript', 'Levar o lixo', 'Ligar para faculdade', 'Fazer Compras']
+let tasks = [
+  "Estudar JavaScript",
+  "Levar o lixo",
+  "Ligar para faculdade",
+  "Fazer Compras",
+];
 
-function generateList() {
-  const list = document.querySelector('.list')
-  for (let i= 0; i < task.length; i++) {
-    const item = document.createElement('li')
-    item.appendChild(document.createTextNode(task[i]))
-    list.appendChild(item)
+function buttonRemoveEventListener() {
+  const buttonsRemove = document.querySelectorAll(".removeItem");
+
+  for (let i = 0; i < buttonsRemove.length; i++) {
+    const buttonRemoveItem = buttonsRemove[i];
+
+    buttonRemoveItem.addEventListener("click", function () {
+      console.log("removeu");
+    });
   }
-  console.log(generateList)
 }
 
-generateList()
+function generateList() {
+  const list = document.querySelector(".list");
+  for (let i = 0; i < tasks.length; i++) {
+    const item = document.createElement("li");
+    const task = tasks[i];
+    item.innerHTML = `<p>${task}</p><button class="removeItem">remove</button>`;
+    list.appendChild(item);
+  }
+  buttonRemoveEventListener();
+}
+
+function removeAllTask() {
+  document.querySelector("ul").innerHTML = "";
+}
+
+function addNewItem() {
+  tasks.push(taskInput.value);
+}
+
+function clearInput() {
+  taskInput.value = "";
+}
+
+function handleSubmit() {
+  addNewItem();
+  clearInput();
+  removeAllTask();
+  generateList();
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  handleSubmit();
+});
+
+generateList();
